3d605f04c1a4a16062f592e8c0a55e41280b7c4f
Grimm06 <standablaze06@gmail.com>
2018-11-04 19:51:50 +0200

    final

---
+++ kleo.js
@@ -0,0 +1,123 @@
+		$(window).load(function(){
+			
+     $('.loader').delay(1300) .fadeOut();
+});
+	var keik = new Audio();
+	keik.src = "a/keik.mp3";
+	function PlaySound(keik) {
+		keik.play();
+	}
+	var vzion = new Audio();
+	vzion.src = "a/vzion.mp3";
+	function PlaySound(vzion) {
+		vzion.play();
+	}
+	var aero = new Audio();
+	aero.src = "a/aero.mp3";
+	function PlaySound(aero) {
+		aero.play();
+	}
+	var kwlos = new Audio();
+	kwlos.src = "a/kwlos.mp3";
+	function PlaySound(kwlos) {
+		kwlos.play();
+	}
+	var satan = new Audio();
+	satan.src = "a/satan.mp3";
+	function PlaySound(satan) {
+		satan.play();
+	}
+	var tsifoutis = new Audio();
+	tsifoutis.src = "a/tsifoutis.mp3";
+	function PlaySound(tsifoutis) {
+		tsifoutis.play();
+	}
+	var katara = new Audio();
+	katara.src = "a/katara.mp3";
+	function PlaySound(katara) {
+		katara.play();
+	}
+	var wish = new Audio();
+	wish.src = "a/wish.mp3";
+	function PlaySound(wish) {
+		wish.play();
+	}
+	var parelasi = new Audio();
+	parelasi.src = "a/parelasi.mp3";
+	function PlaySound(parelasi) {
+		parelasi.play();
+	}
+	var gala = new Audio();
+	gala.src = "a/gala.mp3";
+	function PlaySound(gala) {
+		gala.play();
+	}
+	var mpaza = new Audio();
+	mpaza.src = "a/mpaza.mp3";
+	function PlaySound(mpaza) {
+		mpaza.play();
+	}
+	var worm = new Audio();
+	worm.src = "a/worm.mp3";
+	function PlaySound(worm) {
+		worm.play();
+	}
+	var vita = new Audio();
+	vita.src = "a/vita.mp3";
+	function PlaySound(vita) {
+		vita.play();
+	}
+	var kefali = new Audio();
+	kefali.src = "a/kefali.mp3";
+	function PlaySound(kefali) {
+		kefali.play();
+	}
+	var nero = new Audio();
+	nero.src = "a/nero.mp3";
+	function PlaySound(nero) {
+		nero.play();
+	}
+	var studio = new Audio();
+	studio.src = "a/studio.mp3";
+	function PlaySound(studio) {
+		studio.play();
+	}
+	var evga = new Audio();
+	evga.src = "a/evga.mp3";
+	function PlaySound(evga) {
+		evga.play();
+	}
+	var xoxo = new Audio();
+	xoxo.src = "a/xoxo.mp3";
+	function PlaySound(xoxo) {
+		xoxo.play();
+	}
+	var halle = new Audio();
+	halle.src = "a/halle.mp3";
+	function PlaySound(halle) {
+		halle.play();
+	}
+	var vlameno = new Audio();
+	vlameno.src = "a/vlameno.mp3";
+	function PlaySound(vlameno) {
+		vlameno.play();
+	}
+	var leader = new Audio();
+	leader.src = "a/leader.mp3";
+	function PlaySound(leader) {
+		leader.play();
+	}
+	var aaa = new Audio();
+	aaa.src = "a/aaa.mp3";
+	function PlaySound(aaa) {
+		aaa.play();
+	}
+	async function kappa() {
+		document.body.style.cursor="wait";
+		await sleep(2000);
+		alert("Οι κατάρες και τα αναθέματα έχουν αποσταλεί");
+		document.body.style.cursor="default";
+	}
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
